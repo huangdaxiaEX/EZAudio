10e669ee4f316079020776015a049240d9a28620
huangdaxia <huangkai@tuya.com>
2018-05-08 15:01:48 +0800

    fix apple music bug

--- EZAudio/EZAudioFile.m
+++ EZAudio/EZAudioFile.m
@@ -244,21 +244,8 @@ typedef struct
     // Determine if the file actually exists
     //
     CFURLRef url = self.info->sourceURL;
-//    NSURL *fileURL = (__bridge NSURL *)(url);
-//    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fileURL.path];
-//
-//    //
-//    // Create an ExtAudioFileRef for the file handle
-//    //
-//    if (fileExists)
-//    {
-//        [EZAudioUtilities checkResult:ExtAudioFileOpenURL(url, &self.info->extAudioFileRef)
-//                            operation:"Failed to create ExtAudioFileRef"];
-//    }
-//    else
-//    {
-//        return NO;
-//    }
+    [EZAudioUtilities checkResult:ExtAudioFileOpenURL(url, &self.info->extAudioFileRef)
+                                 operation:"Failed to create ExtAudioFileRef"];
     
     //
     // Get the underlying AudioFileID
